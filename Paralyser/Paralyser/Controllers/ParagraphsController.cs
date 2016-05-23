f1f4cf3a176b116518fe8e37cef6de16168b0271
Richard Banks <rbanks54@msn.com>
2016-05-24 08:05:23 +1000

    Adding EF6 code first and web api

---
+++ Paralyser/Paralyser/Controllers/ParagraphsController.cs
@@ -0,0 +1,23 @@
+﻿using Paralyser.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Paralyser.Controllers
+{
+    public class ParagraphsController : ApiController
+    {
+        public void Post([FromBody]string text)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var p = new ParagraphText() { Text = text };
+                db.Paragraphs.Add(p);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Paralyser/Paralyser/Data/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Paralyser.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base() { }
+
+        public DbSet<ParagraphText> Paragraphs { get; set; }
+    }
+}
